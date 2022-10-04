939a966d7c49783994c86f65724d4f2e1342edae
Denis Gorbachuk <gorbachukDO@gmail.com>
2022-10-04 16:52:31 +0700

    Complite module 4

---
+++ Allfiles/Mod04/Labfiles/01_WorldJourney_begin/WorldJourney/Controllers/CityController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using WorldJourney.Models;
+
+namespace WorldJourney.Controllers
+{
+    public class CityController : Controller
+    {
+        private IData _data;
+        private IHostingEnvironment _environment;
+
+        public CityController(IData data, IHostingEnvironment environment)
+        {
+            _data = data;
+            _environment = environment;
+            _data.CityInitializeData();
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Page"] = "Search city";
+            return View();
+        }
+
+        public IActionResult Details(int? id)
+        {
+            ViewData["Page"] = "Search city";
+            City city = _data.GetCityById(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Title = city.CityName;
+            return View(city);
+        }
+
+        public IActionResult GetImage(int? cityId)
+        {
+            ViewData["Message"] = "display Image";
+            City requestedCity = _data.GetCityById(cityId);
+            if (requestedCity != null)
+            {
+                string webRootpath = _environment.WebRootPath;
+                string folderPath = "\\images\\";
+                string fullPath = webRootpath + folderPath + requestedCity.ImageName;
+                FileStream fileOnDisk = new FileStream(fullPath, FileMode.Open);
+                byte[] fileBytes;
+                using (BinaryReader reader = new BinaryReader(fileOnDisk))
+                {
+                    fileBytes = reader.ReadBytes((int)fileOnDisk.Length);
+                }
+                return File(fileBytes, requestedCity.ImageMimeType);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
--- Allfiles/Mod04/Labfiles/01_WorldJourney_begin/WorldJourney/Startup.cs
+++ Allfiles/Mod04/Labfiles/01_WorldJourney_begin/WorldJourney/Startup.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using WorldJourney.Models;
+using WorldJourney.Filters;
 
 namespace WorldJourney
 {
@@ -16,12 +11,26 @@ namespace WorldJourney
         {
             services.AddMvc();
             services.AddSingleton<IData, Data>();
+            services.AddScoped<LogActionFilterAttribute>();
         }
 
         public void Configure(IApplicationBuilder app)
         {
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                     name: "TravelerRoute",
+                     template: "{controller}/{action}/{name}",
+                     constraints: new { name = "[A-Za-z ]+" },
+                     defaults: new { controller = "Traveler", action = "Index", name = "Katie Bruce" });
+
+                routes.MapRoute(
+                    name: "defaultRoute",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" },
+                    constraints: new { id = "[0-9]+" });
+            });
         }
     }
 }
