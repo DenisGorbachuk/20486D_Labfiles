10c3d21130474c4097c93e0813195719a53bfeb6
Denis Gorbachuk <gorbachukDO@gmail.com>
2022-10-07 22:42:00 +0700

    Complite module 12

--- Allfiles/Mod12/Labfiles/01_ElectricStore_begin/ElectricStore/Startup.cs
+++ Allfiles/Mod12/Labfiles/01_ElectricStore_begin/ElectricStore/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using ElectricStore.Hubs;
 
 namespace ElectricStore
 {
@@ -18,18 +19,31 @@ namespace ElectricStore
             services.AddDbContext<StoreContext>(options =>
                  options.UseSqlite("Data Source=electricStore.db"));
 
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(60);
+            });
+
+            services.AddSignalR();
+
             services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, StoreContext storeContext, IHostingEnvironment environment)
         {
-			
             storeContext.Database.EnsureDeleted();
             storeContext.Database.EnsureCreated();
 
             app.UseStaticFiles();
 
             app.UseNodeModules(environment.ContentRootPath);
+
+            app.UseSession();
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
+            });
 
             app.UseMvc(routes =>
             {
